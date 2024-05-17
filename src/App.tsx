3c070883c6507eda12733d29a21da279ea84755d
Rakhmanin Aleksandr <a.rachmanin@yandex.ru>
2024-05-17 14:31:39 +0600

    Изменил код App, добавил стили.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {useState} from 'react';
+import './App.css';
+import Game from "./Game/Game";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+    const [number, setNumber] = useState<number []>([]);
+    const getRandomBall = () => {
+        const randomBall: number[] = [];
+        let i = 0;
+        do {
+            const count = Math.floor(Math.random() * (36 - 5 + 1)) + 5;
+            if (!randomBall.includes(count)) {
+                randomBall.push(count);
+                i = i + 1;
+            }
+        } while (i < 5);
+        randomBall.sort((a, b) => a - b);
+        setNumber(randomBall);
+    };
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    return (
+        <div className="App">
+            <div className="game-wrapper">
+                <button className="change-btn" onClick={getRandomBall}>New Numbers</button>
+                <div>
+                    <Game num={number[0]}/>
+                    <Game num={number[1]}/>
+                    <Game num={number[2]}/>
+                    <Game num={number[3]}/>
+                    <Game num={number[4]}/>
+                </div>
+            </div>
+        </div>
+    );
+};
 
-export default App
+export default App;
